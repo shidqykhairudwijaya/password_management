d3cd027eda60d919b9d85c7dde9baf44c43bcf85
shidqykhairudwijaya <shidqykhairudwijayaa@gmail.com>
2025-11-26 08:21:52 +0700

    Refactor database connection handling in Koneksi

--- koneksi.java
+++ koneksi.java
@@ -1,25 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Koneksi {
-    private static Connection conn;
 
-    public static Connection getKoneksi() {
-        if (conn == null) {
+public class Database {
+    private static final String URL = "jdbc:mysql://localhost:3306/password_management";
+    private static final String USER = "root";      
+    private static final String PASSWORD = "";       
+
+    private static Connection connection;
+
+    // Membuka koneksi ke database, gunakan fungsi ini di awal ppgram aja
+    public static void openConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database connected.");
+        }
+    }
+
+    // Mengambil objek Connection saat ini
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            openConnection();
+        }
+        return connection;
+    }
+
+   //fungsi untuk menutup koneksi, tutup di akhir program aja
+    public static void closeConnection() {
+        if (connection != null) {
             try {
-                String url = "jdbc:mysql://localhost:3306/password_management";
-                String user = "root";  // sesuaikan
-                String pass = "";      // sesuaikan
-
-                conn = DriverManager.getConnection(url, user, pass);
-                System.out.println("Koneksi sukses");
-
+                connection.close();
+                System.out.println("Database connection closed.");
             } catch (SQLException e) {
-                System.out.println("Koneksi gagal: " + e.getMessage());
+                e.printStackTrace();
             }
         }
-        return conn;
     }
-}
 
+}
